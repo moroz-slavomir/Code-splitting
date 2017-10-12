8d6e9d35a60a1a5659e41b357d4600f6364bb5ee
PetrS2 <PetrS2@kentico.com>
2017-10-12 11:55:07 +0200

    Router: Add login page

--- DemoReactApp/src/components/app/ContentLayout.jsx
+++ DemoReactApp/src/components/app/ContentLayout.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
-import { ROOT } from '../constants/routes';
+import { ROOT } from '../../constants/routes';
 import { Tabs } from './Tabs.jsx';
 import { Content } from './Content.jsx';
-import { SavingStatusLayout} from './app/SavingStatusLayout.jsx';
+import { SavingStatusLayout} from './SavingStatusLayout.jsx';
 
-const Layout = () => (
+const ContentLayout = () => (
     <div className="container">
         <div className="row form-group">
             <div className="col-xs-12">
@@ -33,4 +33,4 @@ const Layout = () => (
     </div>
 );
 
-export { Layout };
+export { ContentLayout };
