sutrkiller/PV247-2017

ReduxForm: Connect Details to redux form

DemoReactApp/src/actions/profile/actionCreators.js
@@ -1,0 +1,8 @@
+import * as actionTypes from '../../constants/actionTypes';
+
+export const editProfileDetails = (details) => ({
+    type: actionTypes.EDIT_PROFILE_DETAILS,
+    payload: {
+        details,
+    }
+});

DemoReactApp/src/constants/actionTypes.js
@@ -12,3 +12,4 @@ export const SHARED_SAVING_STARTED = 'SHARED_SAVING_STARTED';
 export const SHARED_SAVING_FINISHED = 'SHARED_SAVING_FINISHED';
 export const SHARED_RECEIVE_TOKEN = 'SHARED_RECEIVE_TOKEN';
 export const SHARED_INVALIDATE_TOKEN = 'SHARED_INVALIDATE_TOKEN';
+export const EDIT_PROFILE_DETAILS = 'EDIT_PROFILE_DETAILS';

DemoReactApp/src/containers-redux/profile/Details.jsx
@@ -1,0 +1,22 @@
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import { Details } from '../../components/profile/Details.jsx';
+import { editProfileDetails } from '../../actions/profile/actionCreators';
+
+const mapStateToProps = (state) => ({
+    initialValues : state.profile.details,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onSubmit: (details) => dispatch(editProfileDetails(details))
+});
+
+const formConfig = {
+    form: 'DetailsForm',
+};
+
+const stateEnhancer = connect(mapStateToProps, mapDispatchToProps);
+const formEnhancer = reduxForm(formConfig);
+const connectedComponent = stateEnhancer(formEnhancer(Details));
+
+export { connectedComponent as Details };

moroz-slavomir/Code-splitting

DemoReactApp/src/actions/profile/actionCreators.js
@@ -1,0 +1,8 @@
+import * as actionTypes from '../../constants/actionTypes';
+
+export const updateProfileDetails = (details) => ({
+    type: actionTypes.PROFILE_UPDATE_DETAILS,
+    payload: {
+        details,
+    }
+});

DemoReactApp/src/constants/actionTypes.js
@@ -10,5 +10,8 @@ export const TODO_LIST_ITEM_START_DRAGGING = 'TODO_LIST_ITEM_START_DRAGGING';
 export const TODO_LIST_ITEM_STOP_DRAGGING = 'TODO_LIST_ITEM_STOP_DRAGGING';
 export const TODO_LIST_SAVING_STARTED = 'TODO_LIST_SAVING_STARTED';
 export const TODO_LIST_SAVING_FINISHED = 'TODO_LIST_SAVING_FINISHED';
+
+export const PROFILE_UPDATE_DETAILS = 'PROFILE_UPDATE_DETAILS';
+
 export const SHARED_RECEIVE_TOKEN = 'SHARED_RECEIVE_TOKEN';
 export const SHARED_INVALIDATE_TOKEN = 'SHARED_INVALIDATE_TOKEN';

DemoReactApp/src/containers-redux/profile/Details.jsx
@@ -1,0 +1,22 @@
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import { Details } from '../../components/profile/Details.jsx';
+import { updateProfileDetails } from '../../actions/profile/actionCreators';
+
+const mapStateToProps = (state) => ({
+    initialValues : state.profile.details,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onSubmit: (details) => dispatch(updateProfileDetails(details))
+});
+
+const formConfig = {
+    form: 'DetailsForm',
+};
+
+const stateEnhancer = connect(mapStateToProps, mapDispatchToProps);
+const formEnhancer = reduxForm(formConfig);
+const connectedComponent = stateEnhancer(formEnhancer(Details));
+
+export { connectedComponent as Details };