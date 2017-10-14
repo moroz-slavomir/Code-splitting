sutrkiller/PV247-2017

Router: Add title to routes

DemoReactApp/src/components/app/ContentLayout.jsx
@@ -3,9 +3,11 @@ import { Tabs } from './Tabs.jsx';
 import { Content } from './Content.jsx';
 import { SavingStatus } from '../../containers-redux/todo-list/SavingStatus.jsx';
 import { LogoutButton } from '../../containers-redux/app/LogoutButton.jsx';
+import { HeadInHelmet } from '../../containers-redux/app/HeadInHelment.jsx';
 
 const ContentLayout = () => (
     <div className="container">
+        <HeadInHelmet />
         <div className="row">
             <div className="col-xs-5">
                 <h1>TODO List</h1>

DemoReactApp/src/components/login/LoginLayout.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { LoginForm } from '../../containers-redux/login/LoginForm.jsx';
 import * as routes from '../../constants/routes';
+import { HeadInHelmet } from '../../containers-redux/app/HeadInHelment.jsx';
 
 const LoginLayout = ({ from }) => {
     const originalLocation = from || { pathname: routes.ROOT };
 
     return [
+        <HeadInHelmet key="head" />,
         <div className="panel panel-warning" key="cookies">
             <div className="panel-heading text-center">
                 <h5>

DemoReactApp/src/containers-redux/app/HeadInHelment.jsx
@@ -1,0 +1,11 @@
+import { connect } from 'react-redux';
+import { HeadInHelmet} from '../../components/app/HeadInHelmet.jsx';
+
+const mapStateToProps = (state) => ({
+    route: state.router.location.pathname,
+});
+
+const enhancer = connect(mapStateToProps);
+const connectedComponent = enhancer(HeadInHelmet);
+
+export { connectedComponent as HeadInHelmet };

moroz-slavomir/Code-splitting

DemoReactApp/src/components/app/ContentLayout.jsx
@@ -5,9 +5,11 @@ import { Tabs } from './Tabs.jsx';
 import { Content } from './Content.jsx';
 import { SavingStatusLayout} from './SavingStatusLayout.jsx';
 import { LogoutButton } from '../../containers-redux/app/LogoutButton.jsx';
+import { HeadInHelmet } from '../../containers-redux/shared/HeadInHelment.jsx';
 
 const ContentLayout = () => (
     <div className="container">
+        <HeadInHelmet />
         <div className="row">
             <div className="col-xs-5">
                 <h1>TODO List</h1>

DemoReactApp/src/components/login/LoginLayout.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { LoginForm } from '../../containers-redux/login/LoginForm.jsx';
 import * as routes from '../../constants/routes';
+import { HeadInHelmet } from '../../containers-redux/shared/HeadInHelment.jsx';
 
 const LoginLayout = ({ from }) => {
     const originalLocation = from || { pathname: routes.ROOT };
 
     return [
+        <HeadInHelmet key="head" />,
         <div className="panel panel-warning" key="cookies">
             <div className="panel-heading text-center">
                 <h5>

DemoReactApp/src/containers-redux/shared/HeadInHelment.jsx
@@ -1,0 +1,11 @@
+import { connect } from 'react-redux';
+import { HeadInHelmet} from '../../components/shared/HeadInHelmet.jsx';
+
+const mapStateToProps = (state) => ({
+    route: state.router.location.pathname,
+});
+
+const enhancer = connect(mapStateToProps);
+const connectedComponent = enhancer(HeadInHelmet);
+
+export { connectedComponent as HeadInHelmet };