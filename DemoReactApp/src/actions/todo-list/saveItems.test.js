c0565d7c979f7be6ecfe2f7a8a8c388492d27559
Vít Svoboda <vits@kentico.com>
2017-10-18 18:05:08 +0200

    Add mocking to fix the test

--- DemoReactApp/src/actions/todo-list/saveItems.test.js
+++ DemoReactApp/src/actions/todo-list/saveItems.test.js
@@ -20,7 +20,15 @@ test('dispatches actions in the correct order', () => {
     });
 
     const dispatchable = saveItems();
+
+    jest.useFakeTimers();
+    global.localStorage = {
+        setItem: jest.fn()
+    };
+
     dispatchable(dispatch, getState);
+
+    jest.runTimersToTime(1000); // Skip 1 second ahead
 
     expect(dispatch).toHaveBeenCalledWith(savingStarted());
     expect(dispatch).toHaveBeenLastCalledWith(savingFinished());
